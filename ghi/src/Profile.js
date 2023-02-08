d6e1b28dc8625d63411e9bedf427ef67854a41e4
Julie Liao <jliao115@gmail.com>
2023-02-07 16:11:31 -0800

    username greeting on profile page

--- ghi/src/Profile.js
+++ ghi/src/Profile.js
@@ -3,11 +3,34 @@ import { useEffect } from 'react';
 import { useAuthContext, useToken } from './LoginToken';
 
 function Profile(){
-    // const token = useToken()[0];
-    const [currentUser, setCurrentUser] = useState([]);
-    const {token} = useAuthContext(); 
+    const [currentUser, setCurrentUser] = useState({});
 
+    useEffect(() => {
+        async function getUser() {
+            const url = `${process.env.REACT_APP_USERS_API_HOST}/token`
+            const fetchConfig = {
+                credentials: "include",
+            }
+            const response = await fetch(url, fetchConfig);
+            if (response.ok) {
+                const data = await response.json();
+                setCurrentUser(data);
+                // console.log(currentUser);
+            }
+        };
+        getUser(); 
+    }, []);
+  
 
+    return (
+        <div className="container">
+            <div className="page-top">
+                <h3 className="welcome-tag">
+                    Welcome, {currentUser?.user?.first_name}!
+                </h3>
+            </div>
+        </div>
+    );
 }
 
 export default Profile
